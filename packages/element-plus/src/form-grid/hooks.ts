67b682be7441ed3e5b687cf288a8d2b5c171b0a3
hezhengxu2018 <hzx.tony@hotmail.com>
2026-03-01 22:05:16 +0800

    feat(packages/element-plus): 迁移功能至新的grid封装

--- packages/element-plus/src/form-grid/hooks.ts
+++ packages/element-plus/src/form-grid/hooks.ts
@@ -1,12 +1,7 @@
-import type { Grid as IGrid, IGridOptions } from '@silver-formily/grid'
+import type { Grid as IGrid } from '@silver-formily/grid'
 import type { InjectionKey, Ref } from 'vue'
-import { createGrid } from '@silver-formily/grid'
 import { inject } from 'vue'
 
 export const FormGridSymbol: InjectionKey<Ref<IGrid<HTMLElement>>> = Symbol('FormGridContext')
-
-export function createFormGrid(props?: IGridOptions): IGrid<HTMLElement> {
-  return createGrid(props)
-}
 
 export const useFormGrid = (): Ref<IGrid<HTMLElement>> => inject(FormGridSymbol)
