e8d83b268422afd4b42f3f73e81a8b613e07016a
hezhengxu <hzx.tony@hotmail.com>
2026-05-29 13:49:27 +0800

    chore(packages/path): 完善benchmark

--- packages/path/benchmark.ts
+++ packages/path/benchmark.ts
@@ -1,32 +1,33 @@
-import b from 'benny'
-import _ from 'lodash'
+import { times } from 'es-toolkit/compat'
+import { bench, do_not_optimize, run } from 'mitata'
 import { Parser } from './src/parser'
 
 const str
   = 'aakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.bbmmmmmmmmmmmmmmmmmmmmmmmmmmm.cceeeeeeeeeeeeeeeeeee'
 
-b.suite(
-  'Path benchmark',
-  b.add('Path parse', () => {
-    _.times(1e3, () => {
-      new Parser(str).parse()
-    })
-  }),
-  b.add('Normal foreach', () => {
-    _.times(1e3, () => {
-      if (!/[()*[\]]/.test(str)) {
-        str.replace(/\s+/g, '').split('.')
-      }
-    })
-  }),
-  b.add('charCodeAt foreach', () => {
-    _.times(1e3, () => {
-      const res = []
-      for (let i = 0; i < str.length; i++) {
-        res.push(str.charCodeAt(i))
-      }
-    })
-  }),
-  b.cycle(),
-  b.complete(),
-)
+bench('Path parse', () => {
+  times(1e3, () => {
+    do_not_optimize(new Parser(str).parse())
+  })
+})
+
+bench('Normal foreach', () => {
+  times(1e3, () => {
+    if (!/[()*[\]]/.test(str)) {
+      do_not_optimize(str.replace(/\s+/g, '').split('.'))
+    }
+  })
+})
+
+bench('charCodeAt foreach', () => {
+  times(1e3, () => {
+    const res: number[] = []
+    for (let i = 0; i < str.length; i++) {
+      res.push(str.charCodeAt(i))
+    }
+    do_not_optimize(res)
+  })
+})
+
+// eslint-disable-next-line antfu/no-top-level-await
+await run()
